5bca4de62663e824e46f6d598c8e8db0f717eda9
haneul0147 <96038759+haneul0147@users.noreply.github.com>
2022-03-14 12:58:34 +0900

    add

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class Config :
     JWT_SECRET_KEY = 'yh@1234'
    
-    ACCESS_KEY = "AKIAW5QZZAQHEJCUXX5E"
+    ACCESS_KEY = "AKIAW5QZZAQHOZ3G3MO5"
     SECRET_ACCESS = "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"
     
     S3_BUCKET = "hanul0147-project-scooter"
