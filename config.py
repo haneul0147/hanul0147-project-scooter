8239fb3d7ca28502e164f4e61a04f122fa27389d
haneul0147 <96038759+haneul0147@users.noreply.github.com>
2022-03-16 17:52:11 +0900

    add

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config :
     JWT_SECRET_KEY = 'yh@1234'
-   
+    JWT_ACCESS_TOKEN_EXPIRES = False
     ACCESS_KEY = "AKIAW5QZZAQHEJCUXX5E"
     SECRET_ACCESS = "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"
     
