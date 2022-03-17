cf69a8dd66c9a0146f6acb97de10c5f6c027560f
haneul0147 <96038759+haneul0147@users.noreply.github.com>
2022-03-17 12:06:00 +0900

    add

--- resources/posting.py
+++ resources/posting.py
@@ -64,8 +64,29 @@ class FileUpload(Resource):
             except Exception as e :
                 return {'error' : str(e)}
         
-        return {'result' : '잘 저장되었습니다.',
-                'img_url' : app.config['S3_LOCATION']+file.filename}       
+    
+
+        bucket=Config.S3_BUCKET # Cofig에 들어있는 버킷
+        
+        # 'us-east-1' => 내 S3의 리전
+        client=boto3.client('rekognition','us-east-1' ,
+                     aws_access_key_id = Config.ACCESS_KEY,
+                        aws_secret_access_key = Config.SECRET_ACCESS )
+
+        response = client.detect_labels(Image={'S3Object':{'Bucket':bucket,'Name':app.config['S3_LOCATION']+file.filename}},
+        MaxLabels=10)
+
+        print(response['Labels'])
+
+        result = []
+        for label in response['Labels']:
+            label_dict = {}
+            label_dict['Name'] = label['Name']
+            label_dict['Confidence']=label['Confidence']
+            result.append(label_dict)
+
+        return {'result':result}
+           
 
 
 api.add_resource(FileUpload,'/data')
