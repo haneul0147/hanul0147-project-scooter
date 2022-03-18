3c6713e2eb5b713c183de157bae46ab5b0f4b319
haneul0147 <96038759+haneul0147@users.noreply.github.com>
2022-03-18 13:02:27 +0900

    add

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
 class Config :
     JWT_SECRET_KEY = 'yh@1234'
-    JWT_ACCESS_TOKEN_EXPIRES = False
     ACCESS_KEY = "AKIAW5QZZAQHEJCUXX5E"
     SECRET_ACCESS = "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"
     
-    S3_BUCKET = "hanul0147-project-scooter"
+    S3_BUCKET = "hanul0147-project-scooter-kr"
     S3_LOCATION = 'https://{}.s3.amazonaws.com/'.format(S3_BUCKET)
